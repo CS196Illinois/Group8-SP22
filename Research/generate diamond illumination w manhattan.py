e4c96fcfd73cec8fc6c150b1b08ad8b1776b60e9
ziyinw3 <ziyinw3@illinois.edu>
2022-04-06 11:33:34 -0500

    partial implement coordinate location system

--- Research/generate diamond illumination w manhattan.py
+++ Research/generate diamond illumination w manhattan.py
@@ -12,7 +12,7 @@ colorWHITE = (255,255,255)
 colorBLACK = (0, 0, 0)
 colorGRAY = (121, 121, 121)
 
-size = (701,701)
+size = (300,300)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Maze base")
@@ -22,8 +22,8 @@ gameQuit = False
 width = 50
 
 # starting position on maze, top left square is: padding + 0.5 * (block len - character len)
-move_x = 109
-move_y = 109
+move_x = 2
+move_y = 2
 
 location = (move_x, move_y)
 
@@ -53,15 +53,32 @@ while not gameQuit:
                 move_y += 52
 
     # check and update location of mc:
-
+    loc_mc = (move_x//52, move_y//52)
+    print(loc_mc)
 
     # create a black square for every cell on the board except at a certain cell
 
     screen.fill(colorWHITE)
 
+    ticker_x = 0
+    ticker_y = 0
+    cell_loc = []
+
+    # populate coordinates in separate loop
+    # current issue: locations are stored but printed wrong
+    
+    for x in range(0, size[0]//width):
+        for y in range(0, size[1]//width):
+            cell_loc.append((x,y))
+
+    #print(cell_loc)
+
+    # draw rectangles
+
     for x in range(0, size[0]//width):
         for y in range(0, size[1]//width):
             loc_dark100 = ()
+            # implement condition
             rect = pygame.Rect(x * 52, y * 52, 51, 51)
             pygame.draw.rect(screen, colorBLACK, rect)
 
