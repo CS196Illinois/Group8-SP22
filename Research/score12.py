dfd675130e38a1abee4232c543458dff08e7b9aa
gwendolynsl <99754278+gwendolynsl@users.noreply.github.com>
2022-04-27 02:54:59 -0500

    Retry Option

--- Research/score12.py
+++ Research/score12.py
@@ -43,16 +43,84 @@ textRect.center = (200, 200)
 
 
 
-run = True
+
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------------------------------
+#score implementation !!
+#Colors for use
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+ 
+# Assign location
+scoreX = 3
+scoreY = 3
+ 
+# create the display surface object
+# of specific dimension..e(X, Y).
+#use the variable screen
+ 
+ 
+# create a font object.
+# Remeber: 1st parameter is the font file
+# which is present in pygame.
+# 2nd parameter is size of the font
+font = pygame.font.Font('freesansbold.ttf', 12)
+
+
+def show_score(newX, newY):
+    score = font.render("Score: " + str(flowers), True, blue)
+    screen.blit(score, (newX, newY))
+# create a text surface object,
+text = font.render('Score:' + str(flowers), True, green, blue)
+ 
+# I think you need the rectangle if you want the text to show up without this step it hasn't worked !
+# create a rectangular object for the
+# text surface object
+textRect = text.get_rect()
+ 
+# set the center of the rectangular object.
+textRect.center = (200, 200)
+ 
+# Sets up window
 while True:
-    screen.fill(blue)
-    screen.blit(score, textRect)
-    pygame.display.update()
+ 
+    # completely fill the surface object
+    # with white color
+    display_surface.fill(white)
+ 
+    # copying the text surface object
+    # to the display surface object
+    # at the center
+    display_surface.blit(text, textRect)
+ 
     for event in pygame.event.get():
+ 
+        # if event object type is QUIT
+        # then quitting the pygame
+        # and program both.
         if event.type == pygame.QUIT:
+ 
+            # deactivates the pygame library
             pygame.quit()
+ 
+            # quit the program.
             quit()
+ 
+        # Draws the surface object to the screen.
         pygame.display.update()
+
+#-----------------------------------------------------------------------------------------------
+#retry button
+
+
+
 
 
 
